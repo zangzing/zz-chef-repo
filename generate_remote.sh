d07352f9c6741255d111033c195173a5ed576a05
gregseitz <gseitz@gmail.com>
2011-06-29 10:12:15 -0700

    --

--- generate_remote.sh
+++
@@ -1,4 +0,0 @@
-# run the chef scripts locally against the data in dna.json
-#
-# testing with local .json for now
-sudo bundle exec chef-solo -l debug -c chef-local/remote-solo.rb -j chef-local/remote-deploy.json
