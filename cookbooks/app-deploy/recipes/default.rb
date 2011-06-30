e18cab7dc67d4b64998ea67472a7b18154c56639
gregseitz <gseitz@gmail.com>
2011-06-30 15:30:25 -0700

    --

--- cookbooks/app-deploy/recipes/default.rb
+++ cookbooks/app-deploy/recipes/default.rb
@@ -10,6 +10,7 @@ run_for_app(:photos => [:solo,:util,:app,:app_master,:db],
     migration_command "rake db:migrate"
     action :deploy # or :rollback
     before_migrate do
+      zz = node[:zz]
       require "/var/chef/cookbooks/zz-chef-repo/cookbooks/app-deploy/testing/before_migrate.rb"
     end
     before_symlink {}
--- cookbooks/app-deploy/testing/before_migrate.rb
+++ cookbooks/app-deploy/testing/before_migrate.rb
@@ -1,4 +1,4 @@
 puts "-----TEST_BEFORE_MIGRATE------"
-puts node
-puts node[:zz][:app_name]
+puts zz
+puts zz[:app_name]
 puts "-----TEST_BEFORE_MIGRATE------"
