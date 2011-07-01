07ad441094720c0980d17b590a33d70384a7e864
gregseitz <gseitz@gmail.com>
2011-07-01 16:21:25 -0700

    --

--- cookbooks/app-restart/recipes/default.rb
+++ cookbooks/app-restart/recipes/default.rb
@@ -11,95 +11,16 @@ run_for_app(:photos => [:solo,:util,:app,:app_master,:db],
       :current_dir => current_dir,
       :deploy_user => deploy_user,
       :deploy_group => deploy_group,
+      :release_dir => '', #todo get this from fs
       :zz => node[:zz]
   }
   chef_base = ZZDeploy.env.project_root_dir
 
-  deploy base_dir do
-    repo zz[:group_config][:app_git_url]
-    revision zz[:app_deploy_tag]
-    user deploy_user
-    group deploy_group
-    deploy_starting
-    migrate false
-    migration_command "rake db:migrate"
-    action :deploy
-    before_migrate do
-      hv = for_hook
-      hv[:release_dir] = release_path
+  ruby_code = File.open("#{chef_base}/cookbooks/app-restart/helpers/prep_hook_vars.rb", 'r') {|f| f.read }
+  instance_eval(ruby_code)
 
-      # prep vars we want to pass
-      ruby_code = File.open("#{chef_base}/cookbooks/app-deploy/helpers/prep_hook_vars.rb", 'r') {|f| f.read }
-      instance_eval(ruby_code)
-
-      # now our own hook code
-      ruby_code = File.open("#{chef_base}/cookbooks/app-deploy/helpers/before_migrate.rb", 'r') {|f| f.read }
-      instance_eval(ruby_code)
-
-      # and finally the app code if it has a hook in the deploy dir
-      ruby_code = File.open("#{release_path}/deploy/before_migrate.rb", 'r') {|f| f.read } rescue nil
-      #instance_eval(ruby_code) if !ruby_code.nil?
-    end
-    before_symlink do
-      hv = for_hook
-      hv[:release_dir] = release_path
-
-      # prep vars we want to pass
-      ruby_code = File.open("#{chef_base}/cookbooks/app-deploy/helpers/prep_hook_vars.rb", 'r') {|f| f.read }
-      instance_eval(ruby_code)
-
-      # now our own hook code
-      ruby_code = File.open("#{chef_base}/cookbooks/app-deploy/helpers/before_symlink.rb", 'r') {|f| f.read }
-      instance_eval(ruby_code)
-
-      # and finally the app code if it has a hook in the deploy dir
-      ruby_code = File.open("#{release_path}/deploy/before_symlink.rb", 'r') {|f| f.read } rescue nil
-      #instance_eval(ruby_code) if !ruby_code.nil?
-    end
-    before_restart do
-      hv = for_hook
-      hv[:release_dir] = release_path
-
-      # prep vars we want to pass
-      ruby_code = File.open("#{chef_base}/cookbooks/app-deploy/helpers/prep_hook_vars.rb", 'r') {|f| f.read }
-      instance_eval(ruby_code)
-
-      # now our own hook code
-      ruby_code = File.open("#{chef_base}/cookbooks/app-deploy/helpers/before_restart.rb", 'r') {|f| f.read }
-      instance_eval(ruby_code)
-
-      # and finally the app code if it has a hook in the deploy dir
-      ruby_code = File.open("#{release_path}/deploy/before_restart.rb", 'r') {|f| f.read } rescue nil
-      #instance_eval(ruby_code) if !ruby_code.nil?
-    end
-    after_restart do
-      hv = for_hook
-      hv[:release_dir] = release_path
-
-      # prep vars we want to pass
-      ruby_code = File.open("#{chef_base}/cookbooks/app-deploy/helpers/prep_hook_vars.rb", 'r') {|f| f.read }
-      instance_eval(ruby_code)
-
-      # now our own hook code
-      ruby_code = File.open("#{chef_base}/cookbooks/app-deploy/helpers/after_restart.rb", 'r') {|f| f.read }
-      instance_eval(ruby_code)
-
-      # and finally the app code if it has a hook in the deploy dir
-      ruby_code = File.open("#{release_path}/deploy/after_restart.rb", 'r') {|f| f.read } rescue nil
-      #instance_eval(ruby_code) if !ruby_code.nil?
-    end
-    restart_command do
-      hv = for_hook
-      hv[:release_dir] = release_path
-
-      # prep vars we want to pass
-      ruby_code = File.open("#{chef_base}/cookbooks/app-deploy/helpers/prep_hook_vars.rb", 'r') {|f| f.read }
-      instance_eval(ruby_code)
-
-      # now our own hook code
-      ruby_code = File.open("#{chef_base}/cookbooks/app-deploy/helpers/restart_command.rb", 'r') {|f| f.read }
-      instance_eval(ruby_code)
-    end
-  end
+  # now our own restart code (check to see if user has custom code)
+  ruby_code = File.open("#{chef_base}/cookbooks/app-restart/helpers/restart_command.rb", 'r') {|f| f.read }
+  instance_eval(ruby_code)
 
 end
