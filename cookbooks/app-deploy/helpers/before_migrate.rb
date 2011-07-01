9a8e14832c140934c368e08b2205cc4d97b29770
gregseitz <gseitz@gmail.com>
2011-06-30 20:23:23 -0700

    --

--- cookbooks/app-deploy/helpers/before_migrate.rb
+++ cookbooks/app-deploy/helpers/before_migrate.rb
@@ -6,19 +6,22 @@ puts zz_current_dir
 puts zz_release_dir
 puts "-----TEST_BEFORE_MIGRATE------"
 
-cmd = "ls -al #{zz_release_dir}"
-puts cmd
-`#{cmd}`
-cmd = "su -l #{zz_deploy_user} -c 'cd #{zz_release_dir} && bundle install'"
-puts cmd
-puts `#{cmd}`
+#cmd = "ls -al #{zz_release_dir}"
+#puts cmd
+#`#{cmd}`
+#cmd = "su -l #{zz_deploy_user} -c 'cd #{zz_release_dir} && bundle install'"
+#puts cmd
+#puts `#{cmd}`
 
+
+# install the bundle - need to use su since just calling it directly
+# causes it to work out of our chef directory even though we cd to the
+# proper directory.  Must be something about how it looks at the environment
 #
-## install the bundle
-#execute "bundle_install" do
-#  cwd zz_release_dir
-#  user deploy_user
-#  group deploy_group
-#  command "cd #{zz_release_dir} && bundle install && pwd"
-#  action :run
-#end
+execute "bundle_install" do
+  cwd zz_release_dir
+  user deploy_user
+  group deploy_group
+  command "su -l #{zz_deploy_user} -c 'cd #{zz_release_dir} && bundle install'"
+  action :run
+end
