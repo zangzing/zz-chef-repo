8465385016b5bf7c102328f63262b66b37b014c3
gregseitz <gseitz@gmail.com>
2011-07-06 09:24:32 -0700

    --

--- cookbooks/app-restart/recipes/default.rb
+++ cookbooks/app-restart/recipes/default.rb
@@ -18,4 +18,16 @@ run_for_app(:photos => [:solo,:util,:app,:app_master,:db],
     notifies :run, "execute[maint_mode_off]", :immediately
   end
 
+
+  # now register this instance with the elb if we are of the proper type
+  amazon_elb = zz[:group_config][:amazon_elb]
+  if !amazon_elb.empty? && [:solo,:app,:app_master].include?(role)
+    # run in a ruby block so this happens in recipe convergence
+    ruby_block "reload_client_config" do
+      block do
+        elb = Chef::Recipe::ZZDeploy.env.elb
+        elb.register_instances_with_load_balancer(amazon_elb, zz[:instance_id])
+      end
+    end
+  end
 end
