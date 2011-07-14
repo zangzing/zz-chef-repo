102e769b8aec2631ac91b91ccde68d56452b12ad
gregseitz <gseitz@gmail.com>
2011-07-14 13:10:13 -0700

    --

--- handlers/report_handler.rb
+++ handlers/report_handler.rb
@@ -11,15 +11,19 @@ module ZZ
       zz = env.zz
       amazon = env.amazon
       utils = ZZSharedLib::Utils.new(amazon)
+      instances = [ env.this_amazon_instance ]
+      # determine if we were deploying the app or chef
+      state_tag = env.deploy_config? ? :deploy_chef : :deploy_app
       if failed?
         # mark the status of the machine as an error
-        state_tag = env.deploy_config? ? :deploy_chef : :deploy_app
-        instances = [ env.this_amazon_instance ]
         utils.mark_deploy_state(instances, state_tag, ZZSharedLib::Utils::ERROR)
         # +run_status+ is a value object with all of the run status data
         #message << "#{run_status.formatted_exception}\n"
         # Join the backtrace lines. Coerce to an array just in case.
         #message << Array(backtrace).join("\n")
+      else
+        # run was ok so mark state as ready
+        utils.mark_deploy_state(instances, state_tag, ZZSharedLib::Utils::READY)
       end
       puts "Chef run completed on #{node.name}\n"
     end
