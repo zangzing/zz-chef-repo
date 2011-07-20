e1abdbb070b95db5db108d91377c986ad8a8a5a7
gregseitz <gseitz@gmail.com>
2011-07-20 16:39:27 -0700

    turn off sendmail and create sym link from sendmail to ssmtp

--- cookbooks/ssmtp/recipes/default.rb
+++ cookbooks/ssmtp/recipes/default.rb
@@ -1,15 +1,23 @@
 run_for_app(:photos => [:solo,:util,:app,:app_master,:db],
             :rollup => [:solo,:util,:app,:app_master,:db]) do |app_name, role, rails_env|
 
+  service "sendmail" do
+    action [:stop, :disable]
+  end
+
   package "ssmtp" do
     action :install
+  end
+
+  link "/usr/sbin/sendmail" do
+    to "/usr/sbin/ssmtp"
   end
 
   template "/etc/ssmtp/ssmtp.conf" do
     source "ssmtp.conf.erb"
     owner deploy_user
     group deploy_group
-    mode "0644"
+    mode "0640"
   end
 
 end
