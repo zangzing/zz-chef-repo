de936819170e420370b97405417e4ee7944a35a9
gregseitz <gseitz@gmail.com>
2011-07-19 19:20:49 -0700

    --

--- cookbooks/app-restart/helpers/restart_command.rb
+++ cookbooks/app-restart/helpers/restart_command.rb
@@ -1,4 +1,4 @@
 puts "Restart Unicorn Now........."
 
 # call the re/start script
-run "/bin/su - #{zz[:deploy_user]} -c '/data/global/bin/unicorn_start.rb #{zz_rails_env} #{zz_current_dir} /var/run/zz/unicorn_#{zz_app}.pid 60'"
+run "/data/global/bin/unicorn_start.rb #{zz_rails_env} #{zz_current_dir} /var/run/zz/unicorn_#{zz_app}.pid 60"
