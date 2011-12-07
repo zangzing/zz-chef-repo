1faa0caad4cf37bd595eb858e455b8a6afe66454
gregseitz <gseitz@gmail.com>
2011-12-07 15:29:07 -0800

    Try to shut everything down before finishing

--- cookbooks/app-shutdown/helpers/shutdown_command.rb
+++ cookbooks/app-shutdown/helpers/shutdown_command.rb
@@ -1,18 +1,18 @@
-puts "Shutting down all work."
+puts "Preparing to terminate instance - Shutting down all work."
 
 # stop all resque workers
 run "sudo /usr/bin/zzscripts/#{zz_app}_resque_stop_all"
 
 # stop unicorn
 run "sudo monit unmonitor -g unicorn_#{zz_app}"
-run "/usr/bin/zzscripts/unicorn_stop.rb /var/run/zz/unicorn_#{zz_app}.pid 60"
+run "sudo /usr/bin/zzscripts/unicorn_stop.rb /var/run/zz/unicorn_#{zz_app}.pid 60"
 
 # now stop all other monit tasks
 run "sudo monit stop all"
 
-10.times do
-  sleep 3
+6.times do
   put "letting monit jobs stop"
+  sleep 5
 end
 
 
