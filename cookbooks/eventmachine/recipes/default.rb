098e6082dbf2d33309a191685b775637cfe2d4b5
gregseitz <gseitz@gmail.com>
2011-12-17 16:27:16 -0800

    Add monit monitoring of health_check response

--- cookbooks/eventmachine/recipes/default.rb
+++ cookbooks/eventmachine/recipes/default.rb
@@ -2,6 +2,7 @@ run_for_app(:photos => [:solo,:app,:app_master]) do |app_name, role, rails_env|
 
   current_dir = zz_env.current_dir + "/eventmachine"
   num_workers = zz_env.eventmachine_worker_count
+  public_host_name = zz[:public_hostname]
 
   template "/etc/monit.d/eventmachine_#{app_name}.monitrc" do
     owner root_user
@@ -11,6 +12,7 @@ run_for_app(:photos => [:solo,:app,:app_master]) do |app_name, role, rails_env|
     variables({
       :group => deploy_group,
       :user => deploy_user,
+      :public_host_name => public_host_name,
       :num_workers => num_workers,
       :app_name => app_name,
       :current_dir =>  current_dir,
