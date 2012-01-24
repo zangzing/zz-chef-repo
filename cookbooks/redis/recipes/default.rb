8ccc49234568af9ad113838104bebef4062534af
gregseitz <gseitz@gmail.com>
2012-01-23 17:38:19 -0800

    Add cron job to rewrite redis aof file once a day

--- cookbooks/redis/recipes/default.rb
+++ cookbooks/redis/recipes/default.rb
@@ -155,8 +155,8 @@ run_for_app(:photos => [:solo,:util,:app,:app_master,:db,:db_slave,:local]) do |
       # run a cron job once a day to rewrite aof file
       cron "aof_writer" do
         command "/bin/bash -l -c 'redis-cli BGREWRITEAOF >> /data/#{app_name}/shared/log/cron.log 2>&1'"
-        #hour "1"
-        #minute "0"
+        hour "1"
+        minute "0"
         user deploy_user
       end
     end
